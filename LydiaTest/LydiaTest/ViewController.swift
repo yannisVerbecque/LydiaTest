b7e4002c185e717341aab67e1cc2eae59ec4fb5f
Yannis Verbecque <9284080+yannisVerbecque@users.noreply.github.com>
2021-02-02 12:14:44 -0500

    Initial project creation

---
+++ LydiaTest/LydiaTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LydiaTest
+//
+//  Created by Yannis VERBECQUE on 02/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
